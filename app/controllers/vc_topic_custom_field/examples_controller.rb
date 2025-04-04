66a2ed151eaa5088106fa13b97a874e8a6863f3e
Jahan Gagan <jahanggn9@gmail.com>
2025-04-04 12:32:53 +0530

    EDIT: consistent naming

--- app/controllers/vc_topic_custom_field/examples_controller.rb
+++ app/controllers/vc_topic_custom_field/examples_controller.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-module ::VcTopicCustomField
+module ::VCTopicCustomField
   class ExamplesController < ::ApplicationController
     requires_plugin PLUGIN_NAME
 
--- lib/vc_topic_custom_field/engine.rb
+++ lib/vc_topic_custom_field/engine.rb
@@ -1,9 +1,9 @@
 # frozen_string_literal: true
 
-module ::VcTopicCustomField
+module ::VCTopicCustomField
   class Engine < ::Rails::Engine
     engine_name PLUGIN_NAME
-    isolate_namespace VcTopicCustomField
+    isolate_namespace VCTopicCustomField
     config.autoload_paths << File.join(config.root, "lib")
     scheduled_job_dir = "#{config.root}/app/jobs/scheduled"
     config.to_prepare do
